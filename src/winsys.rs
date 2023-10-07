c0933354ab16cb6ee22b3e4313eba9eac0a3aaa5
dbr <dbr.onix@gmail.com>
2023-10-07 11:29:56 +1030

    fmt

--- src/winsys.rs
+++ src/winsys.rs
@@ -118,7 +118,9 @@ where
                 let draw_data = imgui.render();
 
                 // Render it
-                imgui_renderer.render(&gl, &texture_map, &draw_data).unwrap();
+                imgui_renderer
+                    .render(&gl, &texture_map, &draw_data)
+                    .unwrap();
                 windowed_context.swap_buffers().unwrap();
             }
 
